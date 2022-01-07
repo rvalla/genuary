deb422537b41f686bb969529dc860c9c9d5233fc
Rodrigo Valla <rodrigovalla@protonmail.ch>
2022-01-07 17:25:42 -0300

    Minor improvements...

--- code/2022/gen_8.py
+++ code/2022/gen_8.py
@@ -2,6 +2,7 @@ import math
 import random as rd
 from PIL import Image as im
 from gen_util import GenUtil
+from gen_color import GenColor
 from gen_drawing_canvas import DCanvas
 
 ut = GenUtil()
@@ -13,7 +14,7 @@ class Gen8():
 					grading, speed):
 		self.hw = (active_height, active_width)
 		self.margins = margins
-		self.color = color
+		self.color = GenColor(color)
 		self.canvas = DCanvas(self.hw[1] + margins[1] * 2, self.hw[0] + margins[0] * 2, background)
 		self.signal = signal
 		self.signal_width = self.hw[1]
@@ -28,12 +29,12 @@ class Gen8():
 			for s in range(self.signal_width):
 				x = s + self.margins[1]
 				y = signal_h + self.get_signal_y(self.signal, self.signal_resolution * s) + self.margins[0]
-				self.canvas.draw_point(c, (x,y))
+				self.canvas.draw_point(self.color.c, (x,y))
 			signal_h += 1
 			if grading:
-				c = ut.color_grading(c, background, speed)
+				self.color.degrade(background, speed)
 			else:
-				c = ut.move_color(c, 15)
+				self.color.translate(10)
 
 	def get_signal_y(self, signal, angle):
 		y = 0
