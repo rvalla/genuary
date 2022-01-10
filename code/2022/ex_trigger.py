c8a7c34dd1e0a98b6f97b7939f0ecafc7cb5d265
Rodrigo Valla <rodrigovalla@protonmail.ch>
2022-01-10 16:14:33 -0300

    Genuary: Day 9

--- code/2022/ex_trigger.py
+++ code/2022/ex_trigger.py
@@ -3,24 +3,32 @@ from gen_2 import Gen2
 from gen_3 import Gen3
 from gen_4 import Gen4
 from gen_5 import Gen5
+from gen_6 import Gen6
+from gen_7 import Gen7
+from gen_7B import Gen7B
+from gen_8 import Gen8
+from gen_9 import Gen9
+from gen_util import GenUtil
 import random as rd
+
+ut = GenUtil()
 
 #Gen 1
 for i in range(1):
-	name = "gen1_" + str(i + 130)
-	colors = [(160,160,160),(0,0,0),(50,50,50)]
-	p = Gen1(500, 500, [200,400], (255,255,255), colors, [13,0], 1, False)
+	name = "gen1_" + str(i + 1)
+	colors = [(160,160,160,25),(0,0,0,25),(50,50,50,25)]
+	p = Gen1(500, 500, [200,400], (255,255,255), colors, 10, [13,0], 1, False)
 	p.canvas.canvas.show()
 
 #Gen 2
 for i in range(1):
-	name = "gen2_" + str(i + 22)
+	name = "gen2_" + str(i + 1)
 	p = Gen2("assets/input/test.jpg", 50, [100,50], (255,255,255))
 	p.canvas.show()
 
 #Gen 3
 for i in range(1):
-	name = "gen3_" + str(i + 60)
+	name = "gen3_" + str(i + 1)
 	colors = [(160,160,160),(0,0,0),(50,50,50)]
 	t = [0.005,0.03]
 	p = Gen3(500, 500, [50,100], (255,255,255), colors, t, 7, 30)
@@ -28,7 +36,7 @@ for i in range(1):
 
 #Gen 4
 for i in range(1):
-	name = "gen4_" + str(i + 75)
+	name = "gen4_" + str(i + 1)
 	colors = [(160,160,160),(0,0,0),(50,50,50)]
 	angle_var = [i*3.9,i*-0.3]
 	p = Gen4(600,600,[90,60],(255,255,255),colors,0,[300,300],1,8,angle_var,200,False)
@@ -36,14 +44,14 @@ for i in range(1):
 
 #Gen 5
 for i in range(1):
-	name = "gen5_" + str(i + 80)
-	colors = [(160,60,160),(0,0,0),(50,160,50)]
-	p = Gen5(660, 660, [210,630], (255,255,255), colors, [13,0], 1, False, 500, 3)
+	name = "gen5_" + str(i + 1)
+	colors = [(160,60,160,25),(0,0,0,25),(50,160,50,25)]
+	p = Gen5(660, 660, [210,630], (255,255,255), colors, 10, [13,0], 1, False, 500, 3)
 	p.canvas.canvas.show()
 
 #Gen 6
 for i in range(1):
-	name = "gen6_" + str(i + 45)
+	name = "gen6_" + str(i + 1)
 	color = (100,100,100)
 	signal_A = ut.random_signal(3,15,5)
 	signal_B = ut.random_signal(-4,15,4)
@@ -52,22 +60,30 @@ for i in range(1):
 
 #Gen 7
 for i in range(1):
-	name = "gen7_" + str(i + 26)
+	name = "gen7_" + str(i + 1)
 	color = (240,0,100)
 	p = Gen7(600, 600, [100,100], (255,255,255), color, 10, 5)
 	p.canvas.canvas.show()
 
 #Gen 7B
 for i in range(1):
-	name = "gen7B_" + str(i + 5)
+	name = "gen7B_" + str(i + 1)
 	color = (240,0,100)
 	p = Gen7B(600, 600, [100,100], (255,255,255), color, 50, 0)
 	p.canvas.canvas.show()
 
 #Gen 8
 for i in range(1):
-	name = "gen8_" + str(i + 26)
+	name = "gen8_" + str(i + 1)
 	color = (0,0,0)
 	signal = ut.random_signal(2,10,5)
 	p = Gen8(1600, 800, [140,160], (255,255,255), color, signal, 0.01, 15, 34, False, 0.08)
+	p.canvas.canvas.show()
+
+#Gen 9
+for i in range(1):
+	name = "gen9_" + str(i + 1)
+	windows = [(160,160,160,50),(120,120,120,50),(90,90,90,50)]
+	color = [(60,100,190),(100,70,70)]
+	p = Gen9(600, 600, [50,50], (255,255,255), color, 30, windows, 30, False, 10, False)
 	p.canvas.canvas.show()
---
+++ code/2022/gen_9.py
@@ -0,0 +1,86 @@
+import random as rd
+from PIL import Image as im
+from gen_util import GenUtil
+from gen_drawing_canvas import DCanvas
+
+ut = GenUtil()
+
+class Gen9():
+	"The class to draw a minimal skyline..."
+
+	def __init__(self, active_width, active_height, margins, background, color, color_motion, windows_colors,
+					windows_density, constant_windows, buildings, night):
+		self.hw = (active_height, active_width)
+		self.night = night
+		self.margins = margins
+		self.colors = [color[0], color[1]]
+		self.canvas = DCanvas(self.hw[1] + margins[1] * 2, self.hw[0] + margins[0] * 2, background)
+		self.buildings = buildings
+		self.b_widths, self.b_heights = self.building_heights(buildings)
+		self.skyline(buildings, color_motion)
+		self.fill_buildings(buildings, color_motion, windows_colors, constant_windows)
+
+	def skyline(self, buildings, color_motion):
+		x = self.margins[1] + self.hw[1] / 2
+		y = self.margins[0] + self.hw[0] / 2
+		self.canvas.draw_rectangle(self.colors[0], (x,y), (self.hw[1], self.hw[0]))
+		for b in range(1, buildings + 1):
+			x = self.b_widths[b-1] * b - self.b_widths[b-1] / 2 + self.margins[1]
+			y = self.margins[0] + self.hw[0] - self.b_heights[b - 1] / 2
+			self.canvas.draw_rectangle(self.colors[1], (x,y), (self.b_widths[b-1],self.b_heights[b-1]))
+			self.colors[1] = ut.move_color(self.colors[1], color_motion)
+
+	def fill_buildings(self, buildings, color_motion, windows_colors, constant_windows):
+		h_margin = self.b_widths[0] / 10
+		level_height = self.b_widths[0] / 2 - 2 * h_margin
+		start_x = self.margins[1]
+		start_y = 2 * self.margins[0] + self.hw[0]
+		for b in range(buildings):
+			levels = round(self.b_heights[b] // (level_height + h_margin))
+			row = rd.choice([1,2,2,3,3,3,4])
+			self.windows(start_x, start_y, levels, h_margin, level_height, self.b_widths[b], row, color_motion, windows_colors, constant_windows)
+			start_x += self.b_widths[b]
+
+	def windows(self, start_x, start_y, levels, margin, height, total_width, row, color_motion, windows_colors, constant_windows):
+		width = self.windows_widths(margin, total_width, row)
+		floor = self.hw[0] + self.margins[0]
+		density = [rd.randint(3,6),rd.randint(5,10)]
+		color = rd.choice(windows_colors)
+		motion = rd.randint(color_motion // 2, color_motion)
+		factors = [rd.choice([11,13,17,19]),rd.choice([0,0,0,17,19,23])]
+		for r in range(row):
+			x = start_x + margin + r * (width + margin)
+			for l in range(levels):
+				window_on = True
+				if self.night:
+					if rd.random() < 0.5:
+						window_on = False
+				if window_on:
+					y = floor - margin - height - l * (height + margin)
+					ut.gen_rectangle(self.canvas, width, height, [y,x], density, [color], motion, factors, 1, constant_windows)
+
+	def windows_widths(self, margin, total_width, row):
+		width = 0
+		if row == 1:
+			width = total_width - 2 * margin
+		else:
+			width = (total_width - margin) / row - margin
+		return width
+
+	def building_heights(self, buildings):
+		bw = []
+		step = self.hw[1] / buildings
+		for w in range(buildings):
+			bw.append(step)
+		self.hw[1] / buildings
+		bh = []
+		min = self.hw[0] / 3
+		step = self.hw[0] / 9
+		if buildings > 6:
+			heights = 6
+		else:
+			heights = buildings
+		hs = rd.sample(range(0,6),heights)
+		for i in range(buildings):
+			bh.append(hs[i%len(hs)] * step + min)
+		return bw, bh
